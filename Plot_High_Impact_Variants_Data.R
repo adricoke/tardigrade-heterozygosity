5e4ff5664184ff240b3163786adfc9182ee7ae10
adricoke <acoke@unc.edu>
2026-04-30 14:31:34 -0400

    Update Plot_High_Impact_Variants_Data.R

--- Plot_High_Impact_Variants_Data.R
+++ Plot_High_Impact_Variants_Data.R
@@ -42,39 +42,47 @@ fileName <- "select_high_impact_variants.csv"
 high_impact_variants = read.csv(fileName, header=T)
 head(high_impact_variants)
 
-## Protein annotations file(s) (from BLASTp, Uniprot, etc. - not used in final paper figures)
+## Ranked ordered list of genes with high-impact variants based on expression level (from Plot_RNA_Expression_Data.R)
+RNAseq_dir <- "C:/Users/addie/OneDrive - University of North Carolina at Chapel Hill/LAB/tardigrade_heterozygosity/rna_expression_analysis_results"
+fileName <- paste0(RNAseq_dir, "/high_impact_variant_genes_ranked_by_expression.csv")
+high_impact_variant_genes_ranked = read.csv(fileName, header=T)
 
-# fileName <- "../Hypsibius_exemplaris_PredictedProteins.csv"
-fileName <- "../Hypsibius_exemplaris_annotated_genes_with_goslim.csv"
-ProteinAnnotations <- read.csv(fileName, header=T)
-ProteinAnnotations$top_BLASTP_hit <- ifelse(ProteinAnnotations$top_BLASTP_hit == 0, NA, ProteinAnnotations$top_BLASTP_hit)
-ProteinAnnotations$Uniprot_Protein_Annotation <- ifelse(ProteinAnnotations$Uniprot_Protein_Annotation=="Uncharacterized protein", NA, ProteinAnnotations$Uniprot_Protein_Annotation)
-# head(ProteinAnnotations)
-
-# fileName <- "../H_exemplaris_DNA_Repair_Genes_from_Clark-Hachtel_2024_tables_S3_S4.csv"
-# DNA_Repair_Genes <- read.csv(fileName, header=T)
-# head(DNA_Repair_Genes)
+# ## Protein annotations file(s) (from BLASTp, Uniprot, etc. - not used in final paper figures)
+# 
+# # fileName <- "../Hypsibius_exemplaris_PredictedProteins.csv"
+# fileName <- "../Hypsibius_exemplaris_annotated_genes_with_goslim.csv"
+# ProteinAnnotations <- read.csv(fileName, header=T)
+# ProteinAnnotations$top_BLASTP_hit <- ifelse(ProteinAnnotations$top_BLASTP_hit == 0, NA, ProteinAnnotations$top_BLASTP_hit)
+# ProteinAnnotations$Uniprot_Protein_Annotation <- ifelse(ProteinAnnotations$Uniprot_Protein_Annotation=="Uncharacterized protein", NA, ProteinAnnotations$Uniprot_Protein_Annotation)
+# # head(ProteinAnnotations)
+# 
+# # fileName <- "../H_exemplaris_DNA_Repair_Genes_from_Clark-Hachtel_2024_tables_S3_S4.csv"
+# # DNA_Repair_Genes <- read.csv(fileName, header=T)
+# # head(DNA_Repair_Genes)
 
 ###############################################################################
 # Analysis
 ###############################################################################
 
-### Add protein annotations to my data ###
+### Selection relevant columns & add (optional) protein annotations, TPM rankings ###
 mydata <- high_impact_variants %>%
-  left_join(ProteinAnnotations, by = "Gene_ID") %>%
+  # left_join(ProteinAnnotations, by = "Gene_ID") %>%
   # left_join(DNA_Repair_Genes %>% select(Gene_ID, Protein, DNA_Repair_Pathway), by = "Gene_ID") %>%
+  left_join(high_impact_variant_genes_ranked %>% select(gene_id, mean_TPM, genome_percentile_meanTPM), by = c("Gene_ID" = "gene_id")) %>%
   select(
     Gene_ID,
     Annotation,
     AA_pos,
-    Ensembl_Description,
-    top_BLASTP_hit,
-    Uniprot_Protein_Annotation,
-    Uniprot_subcellular_location,
-    Gene_Ontology,
-    GO_slim
+    # Ensembl_Description,
+    # top_BLASTP_hit,
+    # Uniprot_Protein_Annotation,
+    # Uniprot_subcellular_location,
+    # Gene_Ontology,
+    # GO_slim
     # Protein,
-    # DNA_Repair_Pathway
+    # DNA_Repair_Pathway,
+    mean_TPM,
+    genome_percentile_meanTPM
   )
 # head(mydata)
 
@@ -89,6 +97,7 @@ mydata <- mydata %>%
       TRUE ~ "Other high-impact"
     )
   )
+head(mydata)
 
 ###############################################################################
 ### Gene counts with variant(s) per annotation category ###
@@ -143,8 +152,6 @@ nonsense <- mydata %>%
   group_by(Gene_ID) %>%
   slice_min(nonsense_AA_length, n = 1, with_ties = FALSE) %>%
   ungroup() %>%
-  # set Gene_ID as a factor with levels sorted by truncation fraction
-  mutate(Gene_ID = factor(Gene_ID, levels = Gene_ID[order(trunc_frac)])) %>%
   # make new column that is Gene_ID minus prefix (BV898_)
   mutate(Gene_ID_short = sub("BV898_", "", Gene_ID))
 # head(nonsense %>% select(Gene_ID, AA_pos, WT_AA_length, nonsense_AA_length, Uniprot_Protein_Annotation))
@@ -154,12 +161,34 @@ nonsense_significant <- nonsense %>%
   filter(trunc_frac >= 0.25)  # keep only proteins truncated >= 25% (change if needed)
 # head(nonsense_significant %>% select(Gene_ID, AA_pos, WT_AA_length, nonsense_AA_length, trunc_frac))
 
+## set Gene_ID as a factor with levels sorted by truncation fraction OR mean TPM
+nonsense_significant <- nonsense_significant %>%
+  # mutate(Gene_ID_short = factor(Gene_ID_short, levels = Gene_ID_short[order(trunc_frac)]))
+  mutate(Gene_ID_short = factor(Gene_ID_short, levels = Gene_ID_short[order(mean_TPM)]))
+
+# assuming data is sorted by mean_TPM, find the position where mean_TPM drops below 1 to add a threshold line to the plot
+threshold_pos <- nonsense_significant %>%
+  arrange(mean_TPM) %>%
+  mutate(idx = row_number()) %>%
+  summarize(pos = max(idx[mean_TPM < 1])) %>%
+  pull(pos) + 0.5
+y_max <- max(nonsense_significant$WT_AA_length, na.rm = TRUE)
+
 # plot
 ggplot(nonsense_significant)+
   geom_col(aes(x = Gene_ID_short, y = WT_AA_length), fill = "red", width = 0.8)+
   geom_col(aes(x = Gene_ID_short, y = nonsense_AA_length), fill = "darkgrey", width = 0.8)+
   geom_col(aes(x = Gene_ID_short, y = WT_AA_length), color = "black", fill = NA, width = 0.8)+
+  # add dotted line to indicate mean_TPM = 1 threshold (computed above)
+  geom_vline(xintercept = threshold_pos, linetype = "dotted") +  # <- key line
+  # label with mean_TPM
+  geom_text(aes(x = Gene_ID_short, y = WT_AA_length + 50, label = round(mean_TPM, 1)), size = 2.5)+
   coord_flip()+
+  # label dotted line
+  geom_text(aes(x = threshold_pos + 0.7, y = y_max * 0.7),
+            label = "mean TPM > 1", hjust = 0, size = 3) +
+  geom_text(aes(x = threshold_pos - 0.7, y = y_max * 0.7),
+            label = "mean TPM < 1", hjust = 0, size = 3) +
   scale_y_continuous(
     breaks = c(0,500,1000,1500,2000))+
   labs(y = "Nonsense vs Wildtype Protein Length (AA)",
@@ -189,8 +218,6 @@ frameshift <- mydata %>%
   group_by(Gene_ID) %>%
   slice_min(nonsense_AA_length, n = 1, with_ties = FALSE) %>%
   ungroup() %>%
-  # set Gene_ID as a factor with levels sorted by WT_AA_length
-  mutate(Gene_ID = factor(Gene_ID, levels = Gene_ID[order(trunc_frac)])) %>%
   # make new column that is Gene_ID minus prefix (BV898_)
   mutate(Gene_ID_short = sub("BV898_", "", Gene_ID))
 # head(frameshift %>% select(Gene_ID, Gene_ID_short, AA_pos, WT_AA_length, nonsense_AA_length, Uniprot_Protein_Annotation))
@@ -200,12 +227,34 @@ frameshift_significant <- frameshift %>%
   filter(trunc_frac >= 0.25)  # keep only proteins with frameshift >= 25% from C-terminus (change if needed)
 # head(frameshift_significant %>% select(Gene_ID, AA_pos, WT_AA_length, nonsense_AA_length, trunc_frac))
 
+## set Gene_ID as a factor with levels sorted by truncation fraction OR mean TPM
+frameshift_significant <- frameshift_significant %>%
+  # mutate(Gene_ID_short = factor(Gene_ID_short, levels = Gene_ID_short[order(trunc_frac)]))
+  mutate(Gene_ID_short = factor(Gene_ID_short, levels = Gene_ID_short[order(mean_TPM)]))
+
+# assuming data is sorted by mean_TPM, find the position where mean_TPM drops below 1 to add a threshold line to the plot
+threshold_pos <- frameshift_significant %>%
+  arrange(mean_TPM) %>%
+  mutate(idx = row_number()) %>%
+  summarize(pos = max(idx[mean_TPM < 1])) %>%
+  pull(pos) + 0.5
+y_max <- max(frameshift_significant$WT_AA_length, na.rm = TRUE)
+
 # plot
 ggplot(frameshift_significant)+
   geom_col(aes(x = Gene_ID_short, y = WT_AA_length), fill = "blue", width = 0.8)+
   geom_col(aes(x = Gene_ID_short, y = nonsense_AA_length), fill = "darkgrey", width = 0.8)+
   geom_col(aes(x = Gene_ID_short, y = WT_AA_length), color = "black", fill = NA, width = 0.8)+
+  # add dotted line to indicate mean_TPM = 1 threshold (computed above)
+  geom_vline(xintercept = threshold_pos, linetype = "dotted") +  # <- key line
+  # label with mean_TPM
+  geom_text(aes(x = Gene_ID_short, y = WT_AA_length + 50, label = round(mean_TPM, 1)), size = 2.5)+
   coord_flip()+
+  # label dotted line
+  geom_text(aes(x = threshold_pos + 0.7, y = y_max * 0.7),
+            label = "mean TPM > 1", hjust = 0, size = 3) +
+  geom_text(aes(x = threshold_pos - 0.7, y = y_max * 0.7),
+            label = "mean TPM < 1", hjust = 0, size = 3) +
   scale_y_continuous(
     breaks = c(0,500,1000,1500,2000))+
   labs(y = "Pre-frameshift vs Wildtype Protein Length (AA)",
